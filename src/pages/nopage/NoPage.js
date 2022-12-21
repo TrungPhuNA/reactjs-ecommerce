1a967a6b6ca9f55652e5782c377dbf6dd45dcb0c
dungdev <laithedung28563@gmail.com>
2022-12-21 09:16:42 +0700

    fixCSSHeaderFooterHover

--- src/pages/nopage/NoPage.js
+++ src/pages/nopage/NoPage.js
@@ -1,20 +1,25 @@
 import { Outlet } from "react-router-dom";
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
+import "./Nopage.scss";
 
 const NoPage = () => {
   useEffect(() => {
-    window.scrollTo(0, 0)
-  }, [])
-
+    window.scrollTo(0, 0);
+  }, []);
 
   return (
     <>
-      <h1>404 Error</h1>
+      <div className="page_error">
+        <img src={"/iconHeader/404.jpg"} alt="404_Error" />
 
+        <a className="" href="/">
+          Go home!!!
+        </a>
+      </div>
 
       <Outlet />
     </>
-  )
+  );
 };
 
 export default NoPage;
