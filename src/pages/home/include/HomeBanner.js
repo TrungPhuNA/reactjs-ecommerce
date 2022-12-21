e78b3557086bc0df93876c09c47a18044e225aad
dungdev <laithedung28563@gmail.com>
2022-12-21 11:28:58 +0700

    fixCSSHeaderFooterHover

--- src/pages/home/include/HomeBanner.js
+++ src/pages/home/include/HomeBanner.js
@@ -1,42 +1,59 @@
 import React from "react";
 import Images from "../../../components/Image/Images";
-import {Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-function HomeBanner({number}) {
-    return (
-        <div className="cm-width">
-            {number === 4 &&
-                <>
-                    <div className="home-banner--top">
-                        <Link to="/">
-                            <Images src="https://media.hasaki.vn/hsk/1639446173415x130_laneige.jpg" alt="top-1" />
-                        </Link>
-                        <Link to="/">
-                            <Images src="https://media.hasaki.vn/hsk/1640663436415x130_shu%20uemura.jpg" alt="222" />
-                        </Link>
-                        <Link to="/">
-                            <Images src="https://media.hasaki.vn/hsk/1665475978elc_415x130.jpg" alt="333" />
-                        </Link>
-                    </div>
-
-                </>
-            }
-            {number !== 4 &&
-                <>
-                    <div className="home-banner">
-                        <Link to="/">
-                            <Images src="https://media.hasaki.vn/hsk/1639446173415x130_laneige.jpg" alt="23" />
-                        </Link>
-                        <Link to="/">
-                            <Images src="https://media.hasaki.vn/hsk/1640663436415x130_shu%20uemura.jpg" alt="123" />
-                        </Link>
-                        <Link to="/">
-                            <Images src="https://media.hasaki.vn/hsk/1665475978elc_415x130.jpg" alt="123" />
-                        </Link>
-                    </div>
-                </>
-            }
-        </div>
-    )
+function HomeBanner({ number }) {
+  return (
+    <div className="cm-width">
+      {number === 4 && (
+        <>
+          <div className="home-banner--top">
+            <Link to="/">
+              <Images
+                src="https://media.hasaki.vn/hsk/1639446173415x130_laneige.jpg"
+                alt="top-1"
+              />
+            </Link>
+            <Link to="/">
+              <Images
+                src="https://media.hasaki.vn/hsk/1640663436415x130_shu%20uemura.jpg"
+                alt="222"
+              />
+            </Link>
+            <Link to="/">
+              <Images
+                src="https://media.hasaki.vn/hsk/1665475978elc_415x130.jpg"
+                alt="333"
+              />
+            </Link>
+          </div>
+        </>
+      )}
+      {number !== 4 && (
+        <>
+          <div className="home-banner">
+            <Link to="/">
+              <Images
+                src="https://img.freepik.com/premium-psd/banner-sport-shoes-sale-social-media-post-facebook-web-banner-template_70055-854.jpg?w=2000"
+                alt="23"
+              />
+            </Link>
+            <Link to="/">
+              <Images
+                src="https://img.freepik.com/premium-psd/banner-sport-shoes-sale-social-media-post-facebook-web-banner-template_70055-853.jpg?w=2000"
+                alt="123"
+              />
+            </Link>
+            <Link to="/">
+              <Images
+                src="https://mir-s3-cdn-cf.behance.net/project_modules/fs/2bbcfa99737217.5ef9be3dbb9a9.jpg"
+                alt="123"
+              />
+            </Link>
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
-export default HomeBanner
+export default HomeBanner;
